5e4e3eecdc4e011e79aaa49bc4916d0c99e11a78
IvanZhuchkov <zhuchkov.vanya@gmail.com>
2022-07-04 10:40:46 +0300

    finish

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-
+print("Hello, world.")
+n="Ivan."
+print("Hi,", n)
+n=input("What's your name?")
+print("Hi,", n)
+a=float(input("Enter first number:"))
+b=float(input("Enter second number:"))
+c=input("Enter operacion:")
+r=None
+if c=="+":
+    r=a+b
+elif c=="-":
+    r=a-b
+elif c=="*":
+    r= a*b
+elif c=="/":
+    r=a/b
+elif c=="**":
+    r=a**b
+elif c=="%":
+    r=a%b
+elif c=="//":
+    r=a//b
+else :
+    print("Incorect enter")
+if r is not None:
+    print("Result:",r)
